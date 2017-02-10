b42ac4df28616e7e03ff92ad004c6ca968bb1c7b
chong77 <fengchongm@gmail.com>
2017-02-10 17:43:23 +0100

    fait par le prof

---
+++ js/script.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	     <link rel="stylesheet" href="css du site/style.css" />
+
+		<title> Premiere Page </title>						
+	</head>
+	<body>
+		<h1>Explorateur Bitcoin</h1>
+		<h2>Chong Feng </h1>
+		<ul>
+        <li>Chap 1</li>
+        <li>Chap 2</li>
+        <li>Chap 3</li>
+</ul>
+		<h2>Explorateur Bitcoin</h1>
+	</body>
+</html>
